7d21caed6c27f6a99372d87398127babf3905998
david <david@cloudshare.com>
2013-08-16 11:10:16 +0300

    default parameters fix in low level

--- cloudshare/csapi/cslowapi.py
+++ cloudshare/csapi/cslowapi.py
@@ -93,10 +93,10 @@ class CSLowApi(object):
 
             hmac.update(pkey_lower.encode('utf-8'))
             pvalue = params[pkey]
-            query += pkey
+            query += pkey + '='
             if pvalue and len(pvalue) > 0:
                 hmac.update(pvalue.encode('utf-8'))
-                query += '=' + quote(pvalue.encode('utf-8'))
+                query += quote(pvalue.encode('utf-8'))
 
         return (b'https://' + 
                 self.host.encode('utf-8') + 
